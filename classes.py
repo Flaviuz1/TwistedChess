9d4b486de8ddfcc6869185efdc207bcbd508cf0a
Flavius Fiat <flaviusioan.fiat@gmail.com>
2026-02-20 20:28:39 +0200

    Made Assets

--- classes.py
+++ classes.py
@@ -1,44 +1,33 @@
-import numpy as np
+class Board:
+    def __init__(self):
+        self.grid = [
+            ["R0b", "N0b", "B0b", "Q0b", "K0b", "B1b", "N1b", "R1b"],  # black back row
+            ["P0b", "P1b", "P2b", "P3b", "P4b", "P5b", "P6b", "P7b"],  # black pawns
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["   ", "   ", "   ", "   ", "   ", "   ", "   ", "   "],
+            ["P0w", "P1w", "P2w", "P3w", "P4w", "P5w", "P6w", "P7w"],  # white pawns
+            ["R0w", "N0w", "B0w", "Q0w", "K0w", "B1w", "N1w", "R1w"],  # white back row
+        ]
 
-'''
-----Piece Notations----
-Pawn - 1
-Knight - 2
-Bishop - 3
-Rook - 4
-Queen - 5
-King - 6
+    def rotate_board(self):
+        self.grid = [list(row) for row in zip(*self.grid[::-1])]
 
-White - 0
-Black - 1
+class ChessPiece:
+    def __init__(self, type, color, number=None):
+        self.type = type
+        self.color = color
+        self.number = number
+        self.has_moved = False
+        self.pos = self.find_initial_pos()
 
-example : black rook = 41; white queen = 60
-'''
-
-def board(self):
-    self.grid = np.matrix([0,0,0,0,0,0,0,0], # <- black
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0],
-                          [0,0,0,0,0,0,0,0]) # <- white
-
-def chess_piece(self, type, color, moved_flag):
-    self.type = type[0]
-    if self.type != "K" or self.type != "Q":
-        self.number = int(type[1])
-    self.color = color
-    self.has_moved = moved_flag
-    
-    def find_initial_pos(type, color):
-        match type:
-            case "P": return (self.number,                                  (6 if self.color == 0 else 1))
-            case "K": return (self.number + (1 if self.number == 0 else 6), (0 if self.color == 1 else 7))
-            case "B": return (self.number + (2 if self.number == 0 else 5), (0 if self.color == 1 else 7))
-            case "R": return (self.number + (0 if self.number == 0 else 7), (0 if self.color == 1 else 7))
-            case "Q": return (3,                                            (0 if self.color == 1 else 7))
-            case "K": return (4,                                            (0 if self.color == 1 else 7))
-    
-    self.pos = find_initial_pos
+    def find_initial_pos(self):
+        back_row = 7 if self.color == 0 else 0
+        match self.type:
+            case "P": return (self.number, 6 if self.color == 0 else 1)
+            case "R": return (0 if self.number == 0 else 7, back_row)
+            case "N": return (1 if self.number == 0 else 6, back_row)
+            case "B": return (2 if self.number == 0 else 5, back_row)
+            case "Q": return (3, back_row)
+            case "K": return (4, back_row)
